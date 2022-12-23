9385f0260d2104b3477cb49249759d69e6894c22
andLuna <contato.andreluna@gmail.com>
2022-12-23 09:46:11 -0300

    Funcao carrossel carrinho, btn setado

---
+++ js/home.js
@@ -0,0 +1,35 @@
+ // Obtém a lista de todos os cards de produtos
+ const cards = document.querySelectorAll('.card');
+
+ // Oculta todos os cards, exceto os 4 primeiros
+ for (let i = 4; i < cards.length; i++) {
+   cards[i].style.display = 'none';
+ }
+
+ // Adiciona um evento de clique ao botão de avançar
+ document.getElementById('btn-next').addEventListener('click', function() {
+   // Oculta os 4 primeiros cards
+   for (let i = 0; i < 4; i++) {
+     cards[i].style.display = 'none';
+   }
+   // Mostra os próximos 4 cards
+   for (let i = 4; i < 8; i++) {
+     cards[i].style.display = 'inline-block';
+     
+   }
+ });
+
+ // Adiciona um evento de clique ao botão de voltar
+ document.getElementById('btn-prev').addEventListener('click', function() {
+   // Oculta os últimos 4 cards
+   for (let i = 4; i < 8; i++) {
+     cards[i].style.display = 'none';
+   }
+   // Mostra os 4 cards anteriores
+   for (let i = 0; i < 4; i++) {
+     cards[i].style.display = 'inline-block';
+   }
+ });
+
+
+
